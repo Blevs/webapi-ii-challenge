cbff2dfb6efd26da7c2edbc4a22edeb9144d6999
Henry Blevins <heblevi@gmail.com>
2019-04-23 17:32:49 -0400

    Implement updating posts

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -50,4 +50,27 @@ router.delete('/:id', (req, res) => {
     .catch(err => res.status(500).json({error: "The post information could not be retrieved."}));
 });
 
+router.put('/:id', (req, res) => {
+  const newPost = req.body;
+  if (newPost.title && newPost.contents) {
+    db.findById(req.params.id)
+      .then(([post]) => post
+            ? db.update(req.params.id, newPost)
+            .then(updated => updated
+                  ? db.findById(req.params.id)
+                  .then(([post]) => post
+                        ? res.status(200).json(post)
+                        : (void 0).throwError())
+                  .catch(err => res.status(500).json({
+                    error: "The post was modified, but could not be retrieved."
+                  }))
+                  : (void 0).throwError())
+            .catch(err => res.status(500).json({error: "The post could not be modified."}))
+            : res.status(404).json({error: "The post with the specified ID does not exist."}))
+      .catch(err => res.status(500).json({error: "The post information could not be retrieved."}));
+  } else {
+    res.status(500).json({error: "Please provide title and contents for the post."});
+  }
+});
+
 module.exports = router;
