a97a2d582af889bb215147f32728926e20665877
Henry Blevins <heblevi@gmail.com>
2019-04-23 17:24:16 -0400

    Implement post deletion by id

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db.find()
     .then(posts => res.status(200).json(posts))
-    .catch(err => res.staus(500).json({error: "The posts information could not be retrieved."}))
+    .catch(err => res.staus(500).json({error: "The posts information could not be retrieved."}));
 });
 
 router.post('/', (req, res) => {
@@ -24,18 +24,30 @@ router.post('/', (req, res) => {
             })))
       .catch(err => res.status(500).json({
         error: "There was an error while saving the post to the database."
-      }))
+      }));
   } else {
-    res.status(400).json({error: "Please provide title and contents for the post."})
+    res.status(400).json({error: "Please provide title and contents for the post."});
   }
 });
 
 router.get('/:id', (req, res) => {
   db.findById(req.params.id)
-    .then(postArr => postArr.length
-          ? res.status(201).json(postArr[0])
+    .then(([post]) => post
+          ? res.status(201).json(post)
           : res.status(404).json({error: "The post with the specified ID does not exist."}))
-    .catch(err => res.status(500).json({error: "The post information could not be retrieved."}))
+    .catch(err => res.status(500).json({error: "The post information could not be retrieved."}));
+});
+
+router.delete('/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(([post]) => post
+          ? db.remove(req.params.id)
+          .then(deleted => deleted
+                ? res.status(200).json(post)
+                : (void 0).throwError())
+          .catch(err => res.status(500).json({error: "The post could not be removed."}))
+          : res.status(404).json({error: "The post with the specified ID does not exist."}))
+    .catch(err => res.status(500).json({error: "The post information could not be retrieved."}));
 });
 
 module.exports = router;
